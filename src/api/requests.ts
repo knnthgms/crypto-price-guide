e4d344c0efff1fc00c7f9678fcd7a9c48520004b
knnthgms <knnthgms@gmail.com>
2024-12-07 02:01:28 +0530

    Removing mock conditions

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { ListingsResponseData } from './types'
 const API_KEY = import.meta.env.VITE_COINMARKETCAP_API_KEY
 
 const cmcApi = axios.create({
@@ -10,16 +11,15 @@ const cmcApi = axios.create({
   }
 })
 
-// Fetch top cryptocurrencies
 export const fetchTopCryptocurrencies = async (
   limit: number = 50,
   convert: string = 'USD'
-) => {
+): Promise<ListingsResponseData[]> => {
   try {
-    // const response = await cmcApi.get(`/cryptocurrency/listings/latest`, {
-    //   params: { limit, convert }
-    // })
-    // return response.data.data // Return cryptocurrency data
+    const response = await cmcApi.get(`/cryptocurrency/listings/latest`, {
+      params: { limit, convert }
+    })
+    return response.data.data
   } catch (error) {
     console.error('Error fetching top cryptocurrencies:', error)
     throw error
